5faa354f9d45cb49eb114d0faeb37525c52dbd2f
fducau <fnd212@nyu.edu>
2017-02-26 12:14:59 -0500

    Files for task3

---
+++ task3/map.py
@@ -0,0 +1,22 @@
+# !/usr/bin/python
+
+import sys
+import csv
+
+o_header = ['summons_number', 'plate',
+            'license_type',
+            'county', 'state', 'prescint',
+            'issuing_agency', 'violation',
+            'violation_status', 'issue_date',
+            'violation_time', 'judgment_entry_date',
+            'amount_due', 'payment_amount',
+            'penalty_amount', 'fine_amount',
+            'interest_amount', 'reduction_amount']
+
+
+# input comes from STDIN (stream data that goes to the program)
+for entry in csv.reader(sys.stdin, delimiter=','):
+
+    key = entry[o_header.index('license_type')]
+    value = entry[o_header.index('amount_due')]
+    print('{}\t{}'.format(key, value))
---
+++ task3/reduce.py
@@ -0,0 +1,43 @@
+# !/usr/bin/python
+
+import sys
+
+# Expected format if entry coming from
+# parking-violations:
+# plate_id \t amount_due
+
+currentkey = None
+counter = 0
+total = 0
+
+for line in sys.stdin:
+
+    # Remove leading and trailing whitespace
+    line = line.strip()
+
+    # Get key/value
+    key, value = line.split('\t', 1)
+    value = float(value)
+
+    if key != currentkey:
+        # If it is the first key
+        if currentkey is None:
+            total += value
+            counter += 1
+        else:
+            # Generate output
+            print('{}\t{:.2}, {:.2}'.format(currentkey,
+                                            total, total / counter))
+            # Restart counter
+            total = value
+            counter = 1
+
+    else:
+        total += value
+        counter += 1
+
+    currentkey = key
+
+# Compute/output result for the last key
+print('{}\t{:.2}, {:.2}'.format(currentkey,
+                                total, total / counter))
