84d44a35eb5d09610cd5d95ee766e5332bb12fc8
fducau <fnd212@nyu.edu>
2017-02-26 12:41:58 -0500

    Files for task 4

---
+++ task5/map.py
@@ -0,0 +1,24 @@
+# !/usr/bin/python
+
+import sys
+import csv
+
+p_header = ['summons_number', 'issue_date', 'violation_code',
+            'violation_county', 'violation_description',
+            'violation_location', 'violation_precint',
+            'violation_time', 'time_first_observed',
+            'meter_number', 'issuer_code',
+            'issuer_command', 'issuer_precinct',
+            'issuing_agency', 'plate_id', 'plate_type',
+            'registration_state', 'street_name',
+            'vehicle_body_type', 'vehicle_color',
+            'vehicle_make', 'vehicle_year']
+
+
+# input comes from STDIN (stream data that goes to the program)
+for entry in csv.reader(sys.stdin, delimiter=','):
+
+    key1 = entry[p_header.index('plate_id')]
+    key2 = entry[p_header.index('registration_state')]
+    value = '1'
+    print('{}, {}\t{}'.format(key1, key2, value))
---
+++ task5/reduce.py
@@ -0,0 +1,43 @@
+# !/usr/bin/python
+
+import sys
+
+# Expected format if entry coming from
+# parking-violations:
+# plate, state \t #of violations
+
+currentkey = None
+counter = 0
+max_violations = - float('inf')
+
+for line in sys.stdin:
+
+    # Remove leading and trailing whitespace
+    line = line.strip()
+
+    # Get key/value
+    key, value = line.split('\t', 1)
+    value = int(value)
+
+    if key != currentkey:
+        # If it is the first key
+        if currentkey is None:
+            counter += value
+        else:
+            # Keep track of maximum
+            if counter > max_violations:
+                max_key = currentkey
+                max_violations = counter
+            # Restart counter
+            counter = value
+    else:
+        counter += value
+
+    currentkey = key
+
+# Compute/output result for the last key
+if counter > max_violations:
+    max_key = currentkey
+    max_violations = counter
+
+print('{}\t{}'.format(max_key, max_violations))
